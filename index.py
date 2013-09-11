71437292f50155b909eb1f487a75909584c82b19
Pan Xinghua <scupython@gmail.com>
2013-09-11 15:23:31 +0800

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import os
  
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-	    techs = ['Koding', 'Python', 'Tornado', 'AngularJS']
+        techs = ['Koding', 'Python', 'Tornado', 'AngularJS']
         self.render('index.html', techs=techs)
  
 settings = {"template_path": os.path.join(os.path.dirname(__file__), "templates")}
